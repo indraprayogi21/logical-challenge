f7d8bd2e6557b7a8151df671833f0195054fecc4
Indra prayogi <indraprayogi856@gmail.com>
2018-10-06 22:11:26 +0700

    Add files via upload

---
+++ bandingkanangka.js
@@ -0,0 +1,24 @@
+function bandingkanAngka(angka1, angka2) {
+ 
+  if (angka2 > angka1) {
+    return true;
+  } else if (angka1 === angka2) {
+    return -1;
+  } else {
+    return false;
+  }
+}
+
+console.log(bandingkanAngka(5, 8))
+console.log(bandingkanAngka(5, 3)) 
+console.log(bandingkanAngka(4, 4)) 
+console.log(bandingkanAngka(3, 3)) 
+console.log(bandingkanAngka(17, 2))
+
+/* TEST CASES
+console.log(bandingkanAngka(5, 8)); // true
+console.log(bandingkanAngka(5, 3)); // false
+console.log(bandingkanAngka(4, 4)); // -1
+console.log(bandingkanAngka(3, 3)); // -1
+console.log(bandingkanAngka(17, 2)); // false
+*/
